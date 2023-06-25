1f92d66bf08c535ebaa408dc1ed08fe712151ddd
Fox Hui <fox.hui@area28.io>
2023-06-25 20:32:09 +0800

    hi

--- freeCodeCamp_pythonBeginnerProjects/6.tic_tac_toe/game.py
+++ freeCodeCamp_pythonBeginnerProjects/6.tic_tac_toe/game.py
@@ -1,10 +1,25 @@
 from player import Human_Player, Computor_Player
 
+
 class game:
-    board = [" " for i in range(9)]
-
     def __init__(self):
-        pass
+        self.board = [" " for i in range(9)]
+        self.end = False
 
-    def print_board(board):
-        
+    def print_board(self):
+        for row in [self.board[3 * i : (3 * i + 3)] for i in range(3)]:
+            print("| " + " | ".join(row) + " |")
+
+    @staticmethod
+    def print_board_num():
+        for row in [[str(_) for _ in range(9)][3 * i : (3 * i + 3)] for i in range(3)]:
+            print("| " + " | ".join(row) + " |")
+
+    def start(self):
+        self.print_board_num()
+
+        self.print_board()
+
+
+t = game()
+t.start()
