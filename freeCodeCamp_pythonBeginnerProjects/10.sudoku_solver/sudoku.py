956f4062f7601b3e3c7bf7c3ff6f63a28c449807
FoxHui1759 <foxhui1759@gmail.com>
2023-07-06 21:38:50 +0800

    sudoku 20%

--- freeCodeCamp_pythonBeginnerProjects/10.sudoku_solver/sudoku.py
+++ freeCodeCamp_pythonBeginnerProjects/10.sudoku_solver/sudoku.py
@@ -1,10 +1,24 @@
-def is_valid(board: list) -> None:
-    pass
+def is_valid(board: list) -> bool:
+    for i in range(len(board)):
+        if len(board[i]) != len(set(board[i])):
+            return False
+    for j in range(len(board[0])):
+        col_j = list(map(lambda x: x[j], board))
+        if len(col_j) != len(set(col_j)):
+            return False
+    
+    return True
+
+def get_first_empty(board: list) -> bool:
+    for r in range(len(example_board)):
+        for c in range(len(example_board[0])):
+            if example_board[r][c] == "-1"
+            return (r , c)
 
 
 def solve_sudoku(board: list) -> None:
-    if is_valid(board):
-        return True
+    (r, c) = get_first_empty(board)
+    
 
 
 if __name__ == "__main__":
@@ -21,5 +35,7 @@ if __name__ == "__main__":
     ]
     if solve_sudoku(example_board):
         print("It's solvable")
+        for i in range(len(example_board)):
+            print(example_board[i])
     else:
         print("It's not solvable")
