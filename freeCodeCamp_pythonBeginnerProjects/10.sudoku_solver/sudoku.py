ac2bca2c3235e835256822de45562d2a306d9921
FoxHui1759 <foxhui1759@gmail.com>
2023-07-08 10:02:26 +0800

    sudoku finish

--- freeCodeCamp_pythonBeginnerProjects/10.sudoku_solver/sudoku.py
+++ freeCodeCamp_pythonBeginnerProjects/10.sudoku_solver/sudoku.py
@@ -27,7 +27,7 @@ def get_first_empty(board: list) -> bool:
 # assume it's a valid board at the beginning
 def solve_sudoku(board: list) -> bool:
     (r, c) = get_first_empty(board)
-    if not r:
+    if (r, c) == (None, None):
         return True
     for num in range(1, 10):
         if is_valid(board, r, c, num):
